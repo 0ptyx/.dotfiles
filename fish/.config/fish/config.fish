5ac1fc02836acae269a3a5fe4a3e01bfe1a0a997
optyx <optyxx@protonmail.com>
2022-10-02 20:07:59 -0500

    fix command -v to not output

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,11 +2,11 @@ abbr -a l 'ls'
 abbr -a ll 'ls -l'
 abbr -a lll 'ls -la'
 
-if command -v nvim
+if command -v nvim > /dev/null
 	abbr -a e nvim
 else
 	abbr -a e vim
 end
 
-set fish_greeting
+set fish_greeting ""
 set PATH $HOME/.local/bin $PATH
