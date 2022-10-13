fe20ed336d87c74a9c5783b2b2b61a65ba4fb58f
optyx <optyxx@protonmail.com>
2022-10-13 14:35:42 -0400

    Prompt changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,5 +8,18 @@ else
 	abbr -a e vim
 end
 
+function fish_prompt
+    set_color white
+    echo -n "$USER@"
+    echo -n (cat /etc/hostname)
+    echo -n ' '
+    echo -n (string replace "$HOME" "~" $PWD)
+    set_color f5cf65
+    printf '%s ' (__fish_git_prompt)
+    set_color white 
+    echo "\$ "
+end
+
+set fish_command_color 48D597
 set fish_greeting ""
 set PATH $HOME/.local/bin $PATH
